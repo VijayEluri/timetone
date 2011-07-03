249280aba357a266b00846a36277d60a834b3912
Yoshiharu Hirose <yh1224@gmail.com>
2011-07-03 23:16:44 +0900

    再生中に音量が変更されていたら元に戻さない

--- src/net/assemble/timetone/TimetonePlay.java
+++ src/net/assemble/timetone/TimetonePlay.java
@@ -103,15 +103,18 @@ public class TimetonePlay {
             return;
         }
         final int origVol = mAudioManager.getStreamVolume(AudioManager.STREAM_ALARM);
+        final int newVol = TimetonePreferences.getVolume(mCtx);
         mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-                mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM, origVol, 0);
+                if (mAudioManager.getStreamVolume(AudioManager.STREAM_ALARM) == newVol) {
+                    mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM, origVol, 0);
+                }
                 mp.release();
                 g_Mp = null;
             }
         });
-        mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM, TimetonePreferences.getVolume(mCtx), 0);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM, newVol, 0);
         mp.start();
     }
 
